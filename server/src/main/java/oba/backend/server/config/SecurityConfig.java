fc32249633456eb89ac1e54aa7aacbc97e434aa0
Byunjihun <vkvlditm00@naver.com>
2025-08-29 16:25:03 +0900

    feat_#ON-9_Jwt 인증을 위한 Security 설정 추가

--- server/src/main/java/oba/backend/server/config/SecurityConfig.java
+++ server/src/main/java/oba/backend/server/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package oba.backend.server.config;
 
 import lombok.RequiredArgsConstructor;
-import oba.backend.server.jwt.JwtAuthenticationFilter; // import 추가
-import oba.backend.server.jwt.JwtProvider;             // import 추가
+import oba.backend.server.jwt.JwtAuthenticationFilter;
+import oba.backend.server.jwt.JwtProvider;
 import oba.backend.server.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +16,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter; // import 추가
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -24,7 +24,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final CustomUserDetailsService customUserDetailsService;
-    private final JwtProvider jwtProvider; // (1) JwtProvider를 주입받도록 추가
+    private final JwtProvider jwtProvider;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -43,9 +43,6 @@ public class SecurityConfig {
         authProvider.setPasswordEncoder(passwordEncoder());
         return authProvider;
     }
-
-
-    // SecurityConfig.java 파일의 filterChain 메소드를 아래와 같이 수정합니다.
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
