2402482e898b0dfac23a4c6821fe52d23d27a94e
Byunjihun <vkvlditm00@naver.com>
2025-09-15 16:51:58 +0900

    ON-57 코드 중복 방지 및 메시지 중앙 관리를 위한 Enum 추가

--- server/src/main/java/oba/backend/server/common/SuccessMessage.java
+++ server/src/main/java/oba/backend/server/common/SuccessMessage.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 @Getter
 @RequiredArgsConstructor
 public enum SuccessMessage {
+
     SIGNUP_SUCCESS("회원가입이 성공적으로 완료되었습니다."),
     LOGIN_SUCCESS("로그인에 성공했습니다."),
     LOGOUT_SUCCESS("성공적으로 로그아웃되었습니다."),
