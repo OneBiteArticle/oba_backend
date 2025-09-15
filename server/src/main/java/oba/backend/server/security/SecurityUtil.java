7428eb8804d1dfa0b48b6115a67da164c54b91f6
Byunjihun <vkvlditm00@naver.com>
2025-09-15 16:54:29 +0900

    ON-57 재사용성 향상을 위한 SecurityUtil 헬퍼 컴포넌트 추가

--- server/src/main/java/oba/backend/server/security/SecurityUtil.java
+++ server/src/main/java/oba/backend/server/security/SecurityUtil.java
@@ -15,7 +15,7 @@ public class SecurityUtil {
         this.memberRepository = memberRepository;
     }
 
-    public String getCurrentUsername() { // Spring Security 컨텍스트에서는 여전히 'username'으로 통칭됩니다.
+    public String getCurrentUsername() {
         final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null || authentication.getName() == null) {
@@ -26,8 +26,6 @@ public class SecurityUtil {
     }
 
     public Member getCurrentMember() {
-        // --- [변경] findByUsername -> findByEmail ---
-        // Security Context에서 가져온 사용자의 이름(이제 email)을 사용하여 DB에서 Member를 찾습니다.
         String email = getCurrentUsername();
         return memberRepository.findByEmailOrThrow(email);
     }
