bc451f5f9e1666e9caf0b066c9c46ca2e4957cb9
Byunjihun <vkvlditm00@naver.com>
2025-09-25 10:50:17 +0900

    ON-58 Role Enum 추가 - USER/ADMIN 권한 구분

--- server/src/main/java/oba/backend/server/domain/user/Role.java
+++ server/src/main/java/oba/backend/server/domain/user/Role.java
@@ -1,6 +1,23 @@
 package oba.backend.server.domain.user;
 
-public enum Role {
-    USER, ADMIN, NOT_REGISTERED
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
+import java.util.List;
+
+@Getter
+public enum Role {
+    USER("ROLE_USER"),
+    ADMIN("ROLE_ADMIN");
+
+    private final String key;
+
+    Role(String key) {
+        this.key = key;
+    }
+
+    public List<GrantedAuthority> getGrantedAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.key));
+    }
 }
