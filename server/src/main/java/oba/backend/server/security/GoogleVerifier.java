7e4b3a641334657cbc5b082b0c57908e88b91ed3
Staty <kmjs0702@gmail.com>
2025-11-19 17:51:22 +0900

    ON-79 GoogleVerifier 추가 - 구글 idToken 서버측 검증 로직 구현

--- server/src/main/java/oba/backend/server/security/GoogleVerifier.java
+++ server/src/main/java/oba/backend/server/security/GoogleVerifier.java
@@ -1,4 +1,40 @@
 package oba.backend.server.security;
 
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdTokenVerifier;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Collections;
+
+@Component
+@RequiredArgsConstructor
 public class GoogleVerifier {
+
+    @Value("${google.client-id}")
+    private String googleClientId;
+
+    public GoogleIdToken.Payload verify(String idTokenString) {
+        try {
+            GoogleIdTokenVerifier verifier = new GoogleIdTokenVerifier.Builder(
+                    new NetHttpTransport(),
+                    new GsonFactory()
+            )
+                    .setAudience(Collections.singletonList(googleClientId))
+                    .build();
+
+            GoogleIdToken idToken = verifier.verify(idTokenString);
+            if (idToken == null) {
+                throw new RuntimeException("Invalid Google ID Token");
+            }
+
+            return idToken.getPayload();
+
+        } catch (Exception e) {
+            throw new RuntimeException("Google token verification failed", e);
+        }
+    }
 }
