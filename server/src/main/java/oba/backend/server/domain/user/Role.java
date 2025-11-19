393d53336e3e2e0684333dd7b1055098ff598747
Staty <kmjs0702@gmail.com>
2025-11-19 11:51:36 +0900

    ON-67 Role enum 업데이트 - USER/ADMIN 권한 구성 유지

--- server/src/main/java/oba/backend/server/domain/user/Role.java
+++ server/src/main/java/oba/backend/server/domain/user/Role.java
@@ -1,23 +1,6 @@
 package oba.backend.server.domain.user;
 
-import lombok.Getter;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-
-import java.util.List;
-
-@Getter
 public enum Role {
-    USER("ROLE_USER"),
-    ADMIN("ROLE_ADMIN");
-
-    private final String key;
-
-    Role(String key) {
-        this.key = key;
-    }
-
-    public List<GrantedAuthority> getGrantedAuthorities() {
-        return List.of(new SimpleGrantedAuthority(this.key));
-    }
+    USER,
+    ADMIN
 }
