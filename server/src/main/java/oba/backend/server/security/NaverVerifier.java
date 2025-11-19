5830f5b0b82d1a0e47c4d899bfb61a9ada646bec
Staty <kmjs0702@gmail.com>
2025-11-19 17:51:23 +0900

    ON-79 NaverVerifier 추가 - 네이버 accessToken 사용자정보 검증 로직 구현

--- server/src/main/java/oba/backend/server/security/NaverVerifier.java
+++ server/src/main/java/oba/backend/server/security/NaverVerifier.java
@@ -1,4 +1,41 @@
 package oba.backend.server.security;
 
+import lombok.RequiredArgsConstructor;
+import oba.backend.server.security.OAuthAttributes;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Map;
+
+@Component
+@RequiredArgsConstructor
 public class NaverVerifier {
+
+    private final RestTemplate restTemplate = new RestTemplate();
+
+    public OAuthAttributes verify(String accessToken) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.set("Authorization", "Bearer " + accessToken);
+
+        HttpEntity<Void> entity = new HttpEntity<>(headers);
+
+        ResponseEntity<Map> response = restTemplate.exchange(
+                "https://openapi.naver.com/v1/nid/me",
+                HttpMethod.GET,
+                entity,
+                Map.class
+        );
+
+        Map body = (Map) response.getBody().get("response");
+
+        return new OAuthAttributes(
+                (String) body.get("id"),
+                (String) body.get("email"),
+                (String) body.get("name"),
+                (String) body.get("profile_image"),
+                body
+        );
+    }
 }
