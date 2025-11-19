6fde0ccf7323dc933c429b486344fcab343ab1b6
Staty <kmjs0702@gmail.com>
2025-11-19 15:46:14 +0900

    ON-67 IncorrectArticlesId 복합키 매핑 정비

--- server/src/main/java/oba/backend/server/domain/quiz/IncorrectArticlesId.java
+++ server/src/main/java/oba/backend/server/domain/quiz/IncorrectArticlesId.java
@@ -1,13 +1,15 @@
 package oba.backend.server.domain.quiz;
 
 import lombok.*;
-
 import java.io.Serializable;
 
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode
 public class IncorrectArticlesId implements Serializable {
+
     private Long userId;
     private Long articleId;
 }
