9bd0f929ef60c0ba07e06d0dba7bb98da9bad014
Staty <kmjs0702@gmail.com>
2025-11-19 11:50:21 +0900

    ON-67 IncorrectQuiz 엔티티 추가 - 사용자 퀴즈 오답 기록 저장 구조 생성

--- server/src/main/java/oba/backend/server/domain/quiz/IncorrectQuiz.java
+++ server/src/main/java/oba/backend/server/domain/quiz/IncorrectQuiz.java
@@ -1,4 +1,28 @@
 package oba.backend.server.domain.quiz;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "Incorrect_Quiz")
+@IdClass(IncorrectQuizId.class)
 public class IncorrectQuiz {
+
+    @Id
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Id
+    @Column(name = "article_id")
+    private Long articleId;
+
+    private Boolean quiz1;
+    private Boolean quiz2;
+    private Boolean quiz3;
+    private Boolean quiz4;
+    private Boolean quiz5;
 }
