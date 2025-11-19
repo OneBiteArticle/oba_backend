f9ea6381240cf49499a2d5cdbc471586aa101360
Staty <kmjs0702@gmail.com>
2025-11-19 15:46:25 +0900

    ON-67 사용자 권한(Role) Enum 개선

--- server/src/main/java/oba/backend/server/domain/user/Role.java
+++ server/src/main/java/oba/backend/server/domain/user/Role.java
@@ -1,6 +1,16 @@
 package oba.backend.server.domain.user;
 
+import lombok.Getter;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.List;
+
+@Getter
 public enum Role {
     USER,
-    ADMIN
+    ADMIN;
+
+    public List<SimpleGrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_" + this.name()));
+    }
 }
