ac28f1358657f01aa1c30747486f5c4a0c1c47e4
Staty <kmjs0702@gmail.com>
2025-11-19 11:50:18 +0900

    ON-67 IncorrectArticlesId 복합키 추가 - user_id, article_id 기반 복합키 매핑

--- server/src/main/java/oba/backend/server/domain/quiz/IncorrectArticlesId.java
+++ server/src/main/java/oba/backend/server/domain/quiz/IncorrectArticlesId.java
@@ -1,4 +1,13 @@
 package oba.backend.server.domain.quiz;
 
-public class IncorrectArticlesId {
+import lombok.*;
+
+import java.io.Serializable;
+
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class IncorrectArticlesId implements Serializable {
+    private Long userId;
+    private Long articleId;
 }
