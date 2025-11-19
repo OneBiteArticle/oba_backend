0fb5c0080fd14b09d150a6a6154fa4424b5476ff
Staty <kmjs0702@gmail.com>
2025-11-19 15:46:19 +0900

    ON-67 IncorrectQuiz @IdClass 매핑 오류 수정

--- server/src/main/java/oba/backend/server/domain/quiz/IncorrectQuiz.java
+++ server/src/main/java/oba/backend/server/domain/quiz/IncorrectQuiz.java
@@ -2,9 +2,11 @@ package oba.backend.server.domain.quiz;
 
 import jakarta.persistence.*;
 import lombok.*;
+import oba.backend.server.entity.BaseEntity;
 
 @Entity
-@Getter @Setter
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
@@ -20,9 +22,18 @@ public class IncorrectQuiz {
     @Column(name = "article_id")
     private Long articleId;
 
+    @Column(name = "quiz1")
     private Boolean quiz1;
+
+    @Column(name = "quiz2")
     private Boolean quiz2;
+
+    @Column(name = "quiz3")
     private Boolean quiz3;
+
+    @Column(name = "quiz4")
     private Boolean quiz4;
+
+    @Column(name = "quiz5")
     private Boolean quiz5;
 }
