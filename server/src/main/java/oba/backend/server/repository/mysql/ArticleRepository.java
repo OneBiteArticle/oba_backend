02bb1d06d20a0380174bc3eb21c309f05e895b16
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:44 +0900

    ON-79 ArticleRepository 추가

--- server/src/main/java/oba/backend/server/repository/mysql/ArticleRepository.java
+++ server/src/main/java/oba/backend/server/repository/mysql/ArticleRepository.java
@@ -1,6 +1,14 @@
-package oba.backend.server.repository;
+package oba.backend.server.repository.mysql;
 
-import oba.backend.server.domain.mysql.Article;
+import oba.backend.server.entity.mysql.Article;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface ArticleRepository extends JpaRepository<Article, Long> {}
+import java.util.List;
+
+public interface ArticleRepository extends JpaRepository<Article, Long> {
+
+    @Query("SELECT a FROM Article a WHERE a.isUsed = TRUE ORDER BY a.crawlingTime DESC")
+    List<Article> findLatestArticles(Pageable pageable);
+}
