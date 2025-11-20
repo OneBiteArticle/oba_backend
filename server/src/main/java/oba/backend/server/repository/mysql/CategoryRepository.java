7d478158f8d90fedd9e7c6407406a51f883a66d7
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:47 +0900

    ON-79 CategoryRepository 추가

--- server/src/main/java/oba/backend/server/repository/mysql/CategoryRepository.java
+++ server/src/main/java/oba/backend/server/repository/mysql/CategoryRepository.java
@@ -1,4 +1,6 @@
 package oba.backend.server.repository.mysql;
 
-public class CategoryRepository {
-}
+import oba.backend.server.entity.mysql.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {}
