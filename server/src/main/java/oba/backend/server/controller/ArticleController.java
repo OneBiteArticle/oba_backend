ed115be1416082bb847b640316078034b5ec44ab
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:17 +0900

    ON-79 ArticleController 추가 - 기사 조회 API 구현

--- server/src/main/java/oba/backend/server/controller/ArticleController.java
+++ server/src/main/java/oba/backend/server/controller/ArticleController.java
@@ -1,4 +1,23 @@
 package oba.backend.server.controller;
 
+import lombok.RequiredArgsConstructor;
+import oba.backend.server.dto.ArticleSummaryResponse;
+import oba.backend.server.service.ArticleSummaryService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/articles")
+@RequiredArgsConstructor
 public class ArticleController {
+
+    private final ArticleSummaryService articleSummaryService;
+
+    // 홈 화면 최신 기사 조회
+    @GetMapping("/latest")
+    public ResponseEntity<List<ArticleSummaryResponse>> getLatest() {
+        return ResponseEntity.ok(articleSummaryService.getLatestArticles(5));
+    }
 }
