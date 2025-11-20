ff618c5d1f13649a50af2e3a75c02cb51c05ec61
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:38:01 +0900

    ON-79 GptResultService 추가

--- server/src/main/java/oba/backend/server/service/GptResultService.java
+++ server/src/main/java/oba/backend/server/service/GptResultService.java
@@ -1,4 +1,17 @@
 package oba.backend.server.service;
 
+import lombok.RequiredArgsConstructor;
+import oba.backend.server.domain.gpt.GptResult;
+import oba.backend.server.repository.GptResultRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class GptResultService {
+
+    private final GptResultRepository gptResultRepository;
+
+    public GptResult getLatestGptResult() {
+        return gptResultRepository.findTopByOrderByCreatedAtDesc();
+    }
 }
