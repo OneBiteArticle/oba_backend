38ee7b32d40936e3b4d07d2cea11bbd751d33bf7
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:39 +0900

    ON-79 GptMongoRepository 추가

--- server/src/main/java/oba/backend/server/repository/mongo/GptMongoRepository.java
+++ server/src/main/java/oba/backend/server/repository/mongo/GptMongoRepository.java
@@ -1,4 +1,10 @@
 package oba.backend.server.repository.mongo;
 
-public class GptMongoRepository {
+import oba.backend.server.domain.mongo.GptDocument;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.Optional;
+
+public interface GptMongoRepository extends MongoRepository<GptDocument, String> {
+    Optional<GptDocument> findByArticleId(Long articleId);
 }
