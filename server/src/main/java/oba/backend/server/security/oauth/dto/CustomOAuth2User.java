8ed140f832ed424f12ac577f4d2857ffb2a29065
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:52 +0900

    ON-79 CustomOAuth2User 추가

--- server/src/main/java/oba/backend/server/security/oauth/dto/CustomOAuth2User.java
+++ server/src/main/java/oba/backend/server/security/oauth/dto/CustomOAuth2User.java
@@ -1,4 +1,40 @@
 package oba.backend.server.security.oauth.dto;
 
-public class CustomOAuth2User {
+import lombok.Getter;
+import oba.backend.server.domain.user.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+
+import java.util.Collection;
+import java.util.Map;
+
+@Getter
+public class CustomOAuth2User implements OAuth2User {
+
+    private final User user;
+    private final Map<String, Object> attributes;
+
+    public CustomOAuth2User(User user, Map<String, Object> attributes) {
+        this.user = user;
+        this.attributes = attributes;
+    }
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    @Override
+    public Map<String, Object> getAttributes() {
+        return attributes;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getName() {
+        return user.getName();
+    }
 }
