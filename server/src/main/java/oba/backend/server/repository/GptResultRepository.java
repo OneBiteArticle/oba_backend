b287d26d71f6b1541f82cd5a08377b1745a9b88f
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:37 +0900

    ON-79 GptResultRepository 추가

--- server/src/main/java/oba/backend/server/repository/GptResultRepository.java
+++ server/src/main/java/oba/backend/server/repository/GptResultRepository.java
@@ -1,4 +1,9 @@
 package oba.backend.server.repository;
 
-public class GptResultRepository {
+import oba.backend.server.domain.gpt.GptResult;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface GptResultRepository extends MongoRepository<GptResult, String> {
+
+    GptResult findTopByOrderByCreatedAtDesc();
 }
