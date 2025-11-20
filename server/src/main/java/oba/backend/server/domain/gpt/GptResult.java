94efdf7715e56ac40a481d2eeeed43cf2769955c
ByunJihun <vkvlditm00@naver.com>
2025-11-20 16:37:21 +0900

    ON-79 GptResult 엔티티 추가

--- server/src/main/java/oba/backend/server/domain/gpt/GptResult.java
+++ server/src/main/java/oba/backend/server/domain/gpt/GptResult.java
@@ -1,4 +1,25 @@
 package oba.backend.server.domain.gpt;
 
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Document(collection = "gpt_results")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class GptResult {
+
+    @Id
+    private String id;
+
+    private String date;
+    private String title;
+    private String summary;
+    private List<String> keywords;
+    private LocalDateTime createdAt;
 }
